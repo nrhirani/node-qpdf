2692430cf58d2c86672d1bf7b0c4379ef65a1e83
Kyle McNally <kyle@kmcnally.net>
2020-03-16 16:38:42 -0400

    Dep update and dist

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const util_1 = require("util");
-const access = util_1.promisify(require('fs').access);
+const fs_1 = require("fs");
 const execFile = util_1.promisify(require("child_process").execFile);
 const hyphenate = (variable) => variable.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 const execute = async (args) => {
@@ -11,15 +11,11 @@ const execute = async (args) => {
     }
     return child.stdout;
 };
-const fileExists = (input) => {
-    try {
-        access(input, (error) => {
-            if (error)
-                return false;
-            return true;
-        });
+const fileExists = async (file) => {
+    if (fs_1.existsSync(file)) {
+        return true;
     }
-    catch (e) {
+    else {
         return false;
     }
 };
@@ -28,12 +24,9 @@ exports.encrypt = async (input, options, output) => {
         return 'Please specify input file';
     if (!fileExists(input))
         return "Input file doesn't exist";
-    if (output)
-        if (fileExists(output))
-            return "Output file already exists";
     const args = ['--encrypt'];
     if (typeof options.password === 'object') {
-        if (options.password.user === undefined || options.password.owner === undefined) {
+        if (options.password.user === undefined || options.password.user === null || options.password.owner === undefined || options.password.owner === null) {
             return 'Please specify both owner and user passwords';
         }
         args.push(options.password.user);
@@ -43,8 +36,8 @@ exports.encrypt = async (input, options, output) => {
         args.push(options.password);
         args.push(options.password);
     }
-    options.keyLength = options.keyLength || '256';
-    args.push(options.keyLength);
+    options.keyLength = options.keyLength || 256;
+    args.push(options.keyLength.toString());
     if (options.restrictions) {
         if (typeof options.restrictions !== 'object')
             return 'Invalid Restrictions';
