35fd9658e3bbc70501da6ba23c5f82c8ede237bd
Kyle McNally <kyle@kmcnally.net>
2020-05-18 16:17:55 -0400

    Cleanup and linting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-import { promisify } from 'util';
-import { existsSync } from 'fs';
+/* eslint-disable no-param-reassign */
+import { promisify } from "util";
+import { existsSync } from "fs";
+import { execFile } from "child_process";
+
 interface Restrictions {
   print?: "y" | "n" | "full" | "low" | "none";
   modify?: "y" | "n" | "all" | "annotate" | "form" | "assembly" | "none";
@@ -20,98 +23,112 @@ export interface QPdfOptions {
   restrictions?: Restrictions;
 }
 
-const execFile = promisify(require("child_process").execFile);
+const execFilePromise = promisify(execFile);
 
-const hyphenate = (variable: string): string => variable.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+const hyphenate = (variable: string): string =>
+  variable.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 
-const execute = async (args: string[]): Promise<string> => {
-  const child = await execFile('qpdf', args);
-  if(child.stderr) {
-    throw child.stderr;
+const execute = async (callArguments: string[]): Promise<string> => {
+  const { stderr, stdout } = await execFilePromise("qpdf", callArguments);
+  if (stderr) {
+    throw new Error(stderr);
   }
-  return child.stdout;
-}
+  return stdout;
+};
 
-const fileExists = async (file: string): Promise<boolean> => {
-  if(existsSync(file)){
+const fileExists = (file: string): boolean => {
+  if (existsSync(file)) {
     return true;
-  } else {
-    return false;
   }
-}
+  return false;
+};
 
-export const encrypt = async (input: string, options: QPdfOptions, output ?: string): Promise<string> => {
-  if (!input) return 'Please specify input file';
+export const encrypt = async (
+  input: string,
+  options: QPdfOptions,
+  output?: string
+): Promise<string> => {
+  if (!input) return "Please specify input file";
   if (!fileExists(input)) return "Input file doesn't exist";
-  //if (output) if (fileExists(output)) return "Output file already exists";
+  // if (output) if (fileExists(output)) return "Output file already exists";
 
-  const args = ['--encrypt'];
+  const callArguments = ["--encrypt"];
 
   // Set user-password and owner-password
-  if(typeof options.password === 'object') {
-    if(options.password.user === undefined || options.password.user === null || options.password.owner === undefined || options.password.owner === null) {
-      return 'Please specify both owner and user passwords';
+  if (typeof options.password === "object") {
+    if (
+      options.password.user === undefined ||
+      options.password.user === null ||
+      options.password.owner === undefined ||
+      options.password.owner === null
+    ) {
+      return "Please specify both owner and user passwords";
     }
-    args.push(options.password.user);
-    args.push(options.password.owner);
+    callArguments.push(options.password.user);
+    callArguments.push(options.password.owner);
   } else {
     // Push twice for user-password and owner-password
-    args.push(options.password);
-    args.push(options.password);
+    callArguments.push(options.password);
+    callArguments.push(options.password);
   }
 
   // Defaults encryption to AES 256
   options.keyLength = options.keyLength || 256;
 
   // Specifying the key length
-  args.push(options.keyLength.toString());
+  callArguments.push(options.keyLength.toString());
 
   // Add Resctrictions for encryption
   if (options.restrictions) {
-    if (typeof options.restrictions !== 'object') return 'Invalid Restrictions';
+    if (typeof options.restrictions !== "object") return "Invalid Restrictions";
 
-    const restrictions = options.restrictions;
+    const { restrictions } = options;
 
-    for(const restriction in options.restrictions) {
-      const value = (restrictions[restriction] !== '') ? '=' + restrictions[restriction] : '';
-      args.push('--' + hyphenate(restriction) + value);
+    for (const restriction of options.restrictions) {
+      const value =
+        restrictions[restriction] !== "" ? `=${restrictions[restriction]}` : "";
+      callArguments.push(`--${hyphenate(restriction)}${value}`);
     }
   }
 
   // Marks end of --encrypt options
-  args.push('--');
+  callArguments.push("--");
 
   // Input file path
-  args.push(input.replace(/(["\s'$`\\])/g,'\\$1'));
+  callArguments.push(input.replace(/([\s"$'\\`])/g, "\\$1"));
 
   if (output) {
-    args.push(output);
+    callArguments.push(output);
   } else {
-  // Print PDF on stdout
-    args.push('-');
+    // Print PDF on stdout
+    callArguments.push("-");
   }
   // Execute command and return stdout for pipe
-  return execute(args);
+  return execute(callArguments);
 };
 
-export const decrypt = async (input: string, password: string, output ?: string): Promise<string> => {
-  if (!input) return 'Please specify input file';
-  if (!password) return 'Password missing';
+export const decrypt = async (
+  input: string,
+  password: string,
+  output?: string
+): Promise<string> => {
+  if (!input) return "Please specify input file";
+  if (!password) return "Password missing";
 
-  const args = ['--decrypt'];
+  const callArguments = ["--decrypt"];
 
   // Password
-  args.push('--password=' + password.replace(/(["\s'$`\\])/g,'\\$1'));
+  callArguments.push(`--password=${password.replace(/([\s"$'\\`])/g, "\\$1")}`);
 
   // Input file path
-  args.push(input.replace(/(["\s'$`\\])/g,'\\$1'));
+  callArguments.push(input.replace(/([\s"$'\\`])/g, "\\$1"));
 
   // Print PDF on stdout
   if (output) {
-    args.push(output);
+    callArguments.push(output);
   } else {
-    args.push('-');
+    callArguments.push("-");
   }
 
-  return execute(args);
+  return execute(callArguments);
 };
