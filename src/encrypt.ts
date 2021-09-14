091eec64ec7b8df2d42c10f72584d800db4e0f6d
Sparticuz <kyle@kmcnally.net>
2021-09-14 10:23:21 -0400

    Update encrypt to use better support the EncryptSettings object
    This will allow optional passwords, but also adds better support for default settings. Also support for _not_ overwriting the destination file. (Defaults to overwrite)

--- src/encrypt.ts
+++ src/encrypt.ts
@@ -1,65 +1,72 @@
 import execute from "./spawn";
 import { fileExists, hyphenate } from "./utils";
 
-interface Restrictions {
-  accessibility?: "y" | "n";
-  annotate?: "y" | "n";
-  extract?: "y" | "n";
-  modify?: "y" | "n" | "all" | "annotate" | "form" | "assembly" | "none";
-  print?: "y" | "n" | "full" | "low" | "none";
-  useAes?: "y" | "n";
-}
-export interface QPdfOptions {
+const EncryptDefaults = {
+  keyLength: 256,
+  overwrite: true,
+};
+export interface EncryptOptions {
+  input: string;
   keyLength?: number;
-  password:
+  output?: string;
+  overwrite?: boolean | undefined;
+  password?:
     | string
     | {
         owner: string;
         user: string;
       };
-  restrictions?: Restrictions;
+  restrictions?: {
+    accessibility?: "y" | "n";
+    annotate?: "y" | "n";
+    extract?: "y" | "n";
+    modify?: "y" | "n" | "all" | "annotate" | "form" | "assembly" | "none";
+    print?: "y" | "n" | "full" | "low" | "none";
+    useAes?: "y" | "n";
+  };
 }
 
-export default async (
-  input: string,
-  options: QPdfOptions,
-  output?: string
-  // eslint-disable-next-line sonarjs/cognitive-complexity
-): Promise<string> => {
-  if (!input) return "Please specify input file";
-  if (!fileExists(input)) return "Input file doesn't exist";
-  // if (output) if (fileExists(output)) return "Output file already exists";
+// eslint-disable-next-line sonarjs/cognitive-complexity
+export default async (userPayload: EncryptOptions): Promise<Buffer> => {
+  // Set Defaults
+  const payload = { ...EncryptDefaults, ...userPayload };
+
+  // check if the file exists
+  if (!payload.input) throw new Error("Please specify input file");
+  if (!fileExists(payload.input)) throw new Error("Input file doesn't exist");
+  if (payload.output && !payload.overwrite && fileExists(payload.output))
+    throw new Error("Output file already exists");
 
   const callArguments = ["--encrypt"];
 
   // Set user-password and owner-password
-  if (typeof options.password === "object") {
+  if (typeof payload.password === "object") {
     if (
-      options.password.user === undefined ||
-      options.password.user === null ||
-      options.password.owner === undefined ||
-      options.password.owner === null
+      payload.password.user === undefined ||
+      payload.password.user === null ||
+      payload.password.owner === undefined ||
+      payload.password.owner === null
     ) {
-      return "Please specify both owner and user passwords";
+      throw new Error("Please specify both owner and user passwords");
     }
-    callArguments.push(options.password.user, options.password.owner);
-  } else {
+    callArguments.push(payload.password.user, payload.password.owner);
+  } else if (typeof payload.password === "string") {
     // Push twice for user-password and owner-password
-    callArguments.push(options.password, options.password);
+    callArguments.push(payload.password, payload.password);
+  } else {
+    // no password specified, push two empty strings (https://stackoverflow.com/a/43736897/455124)
+    callArguments.push('""', '""');
   }
 
-  // Defaults encryption to AES 256
-  // eslint-disable-next-line no-param-reassign
-  options.keyLength = options.keyLength || 256;
-
   // Specifying the key length
-  callArguments.push(options.keyLength.toString());
+  callArguments.push(payload.keyLength.toString());
 
   // Add Restrictions for encryption
-  if (options.restrictions) {
-    if (typeof options.restrictions !== "object") return "Invalid Restrictions";
-    for (const [restriction, value] of Object.entries(options.restrictions)) {
-      if (restriction === "useAes" && options.keyLength === 256) {
+  if (payload.restrictions) {
+    if (typeof payload.restrictions !== "object")
+      throw new Error("Invalid Restrictions");
+    for (const [restriction, value] of Object.entries(payload.restrictions)) {
+      if (restriction === "useAes" && payload.keyLength === 256) {
         // use-aes is always on with 256 bit keyLength
       } else {
         callArguments.push(`--${hyphenate(restriction)}=${value as string}`);
@@ -68,10 +75,10 @@ export default async (
   }
 
   // Marks end of --encrypt options, Input file path
-  callArguments.push("--", input);
+  callArguments.push("--", payload.input);
 
-  if (output) {
-    callArguments.push(output);
+  if (payload.output) {
+    callArguments.push(payload.output);
   } else {
     // Print PDF on stdout
     callArguments.push("-");
