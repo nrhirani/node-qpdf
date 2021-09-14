66bfd25515fe9de5482e616a482d1002d3c0184a
Sparticuz <kyle@kmcnally.net>
2021-09-14 10:21:00 -0400

    Refactor decrypt to take a DecryptSettings object
    This also makes the password optional. and changes errors to throws

--- src/decrypt.ts
+++ src/decrypt.ts
@@ -1,26 +1,31 @@
 import { fileExists } from "./utils";
 import execute from "./spawn";
 
-export default async (
-  input: string,
-  password: string,
-  output?: string
-): Promise<string> => {
-  if (!input) return "Please specify input file";
-  if (!password) return "Password missing";
-  if (!fileExists(input)) return "Input file doesn't exist";
+export interface DecryptSettings {
+  input: string;
+  output?: string;
+  password?: string;
+}
+
+export default async (payload: DecryptSettings): Promise<Buffer> => {
+  if (!payload.input) throw new Error("Please specify input file");
+  if (!fileExists(payload.input)) throw new Error("Input file doesn't exist");
 
   const callArguments = ["--decrypt"];
 
   // Password
-  callArguments.push(`--password=${password}`);
+  if (payload.password) {
+    callArguments.push(`--password=${payload.password}`);
+  } else {
+    callArguments.push('--password=""');
+  }
 
   // Input file path
-  callArguments.push(input);
+  callArguments.push(payload.input);
 
   // Print PDF on stdout
-  if (output) {
-    callArguments.push(output);
+  if (payload.output) {
+    callArguments.push(payload.output);
   } else {
     callArguments.push("-");
   }
