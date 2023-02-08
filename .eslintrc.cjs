501ba72ede2dd6993816fac9c72445c4e9439ff9
Sparticuz <kyle@kmcnally.net>
2023-02-08 15:37:32 -0500

    breaking major feature: convert to esm

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, n/no-unpublished-require
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   extends: ["@sparticuz/eslint-config"],
   parserOptions: {
-    ecmaVersion: 2021,
+    ecmaVersion: 2022,
     project: "./tsconfig.json",
     sourceType: "module",
   },
